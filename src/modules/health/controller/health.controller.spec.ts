3955bd7d64b36a4a13d34bd0399ac0c3ac22fff0
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2024-05-14 20:16:10 -0300

    health: tests

--- src/modules/health/controller/health.controller.spec.ts
+++ src/modules/health/controller/health.controller.spec.ts
@@ -1,7 +1,24 @@
+import { HealthController } from "./health.controller";
+import { Test, TestingModule } from "@nestjs/testing";
+
 describe("HealthController", () => {
+  let healthController: HealthController;
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      controllers: [HealthController],
+    }).compile();
+
+    healthController = module.get<HealthController>(HealthController);
+  });
+
   describe("GET /health", () => {
+    it("Should be defined", async () => {
+      expect(healthController).toBeDefined();
+    });
+
     it("should return 200", async () => {
-        expect(true).toBe(true)
-    })
+      expect(healthController.healthCheck()).toBe("");
+    });
   });
 });
