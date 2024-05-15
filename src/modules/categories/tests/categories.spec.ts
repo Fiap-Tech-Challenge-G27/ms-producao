ab147316df985e914f002faf25ea0d5e772ef951
Matheus de Mello Vieira <mellomatheus67@gmail.com>
2024-05-14 23:09:58 -0300

    writted categories test

--- src/modules/categories/tests/categories.spec.ts
+++ src/modules/categories/tests/categories.spec.ts
@@ -15,24 +15,22 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { CategoryEntity } from "../core/category.entity";
 import { Category } from "../infra/typeorm/entities/category";
 import { Product } from "@modules/products/infra/typeorm/entities/product";
+import { Repository } from "typeorm";
+import { getRepositoryToken } from "@nestjs/typeorm";
+import { CreateCategoryDto } from "../dtos/create-category.dto";
 
-describe("Categories", () => {
+describe("/categories", () => {
   let categoriesController: CategoriesController;
+  let repositoryMock: Repository<Category>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [CategoriesController],
-      imports: [
-        TypeOrmModule.forRoot({
-          type: "better-sqlite3",
-          database: ":memory:",
-          dropSchema: true,
-          entities: [Category, Product],
-          synchronize: true,
-        }),
-        TypeOrmModule.forFeature([Category, Product]),
-      ],
       providers: [
+        {
+          provide: getRepositoryToken(Category),
+          useClass: Repository,
+        },
         {
           provide: ICategoryRepository,
           useClass: CategoryRepository,
@@ -51,7 +49,39 @@ describe("Categories", () => {
 
     categoriesController =
       module.get<CategoriesController>(CategoriesController);
+    repositoryMock = module.get<Repository<Category>>(
+      getRepositoryToken(Category)
+    );
   });
 
-  it("", async () => {});
+  it("should be defined", () => {
+    expect(categoriesController).toBeDefined();
+  });
+
+  describe("/create", () => {
+    it("should call the repository", async () => {
+
+      const createCategoryDto = {
+        name: "dessert",
+        slug: "dessert",
+        description: "sweets and cakes",
+      }
+
+      const category = {
+        ...createCategoryDto,
+        createdAt: new Date("2019-01-16"),
+        updatedAt: new Date("2019-01-16"),
+        products: undefined,
+        id: undefined
+      }
+
+      jest.spyOn(repositoryMock, "save").mockResolvedValueOnce(category);
+      jest.spyOn(repositoryMock, "findOne").mockReturnValueOnce(null)      
+
+      const response = await categoriesController.create(createCategoryDto as CreateCategoryDto);
+
+      expect(response).toEqual(category)
+      expect(response.constructor).toBe(CategoryEntity)     
+    });
+  });
 });
